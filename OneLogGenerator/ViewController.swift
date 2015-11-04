2e05ebc77ceecc918a8136879231a346138a6ece
ertrue <zhanghongbo1209@gmail.com>
2015-11-03 18:44:23 -0800

    Initial Commit

---
+++ OneLogGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneLogGenerator
+//
+//  Created by Hongbo Zhang on 11/3/15.
+//  Copyright © 2015 Hongbo Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
